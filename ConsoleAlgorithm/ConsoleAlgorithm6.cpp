747cbe94c3f5c1571c516688a94162ca902dd021
JeongJunSoo94 <103792742+JeongJunSoo94@users.noreply.github.com>
2022-06-22 17:52:05 +0900

    today

--- ConsoleAlgorithm/ConsoleAlgorithm6.cpp
+++ ConsoleAlgorithm/ConsoleAlgorithm6.cpp
@@ -341,114 +341,221 @@
 //	shellSort(list,7);
 //	return 0;
 //}
+//#include <iostream>
+//#include "StopWatch.h"
+//void insertionSort(int v[], int left, int right, int gap)
+//{
+//	int i, j, temp;
+//
+//	for (i = left + gap; i <= right; i =i + gap)
+//	{
+//		temp = v[i];
+//		for (j = i - gap; j >= left && v[j] > temp; j = j- gap)
+//		{
+//			v[j + gap] = v[j];
+//		}
+//		v[j + gap] = temp;
+//	}
+//}
+//
+//void shellSort(int v[], int size)
+//{
+//	int gap;
+//	for (gap = size / 2; gap > 0; gap= gap / 2)
+//	{
+//		if ((gap % 2) == 0)
+//		{
+//			gap++;
+//		}
+//		for (int i = 0; i < gap; i++)
+//		{
+//			insertionSort(v, i, size - 1, gap);
+//		}
+//		
+//	}
+//}
+//
+//void quick(int a[],int left,int right)
+//{
+//	int pl = left;
+//	int pr = right;
+//	int x = a[(pl + pr) / 2];
+//	do {
+//		while (a[pl] < x) pl++;
+//		while (a[pr] > x) pr--;
+//		if (pl <= pr) {
+//			int temp=a[pl];
+//			a[pl] = a[pr];
+//			a[pr] = temp;
+//			pl++;
+//			pr--;
+//		}
+//	} while (pl <= pr);
+//	if (left < pr) quick(a,left,pr);
+//	if (pl < right) quick(a, pl, right);
+//}
+//
+//void QuickSort(int list[], int left,int right)
+//{
+//	if (left >= right)
+//	{
+//		return;
+//	}
+//	else
+//	{
+//		int pivotLeft = left+1;
+//		int pivot = left;
+//		int pivotRight = right;
+//		int current = 1;
+//		do
+//		{
+//			if (current)
+//			{
+//				if (list[pivot] >= list[pivotRight])
+//				{
+//					int temp = list[pivot];
+//					list[pivot] = list[pivotRight];
+//					list[pivotRight] = temp;
+//					pivot = pivotRight;
+//					current = 0;
+//				}
+//				--pivotRight;
+//			}
+//			else
+//			{
+//				if (list[pivot] <= list[pivotLeft])
+//				{
+//					int temp = list[pivot];
+//					list[pivot] = list[pivotLeft];
+//					list[pivotLeft] = temp;
+//					pivot = pivotLeft;
+//					current = 1;
+//				}
+//				++pivotLeft;
+//			}
+//		} while (pivotLeft <= pivotRight);
+//		QuickSort(list, left, pivot - 1);
+//		QuickSort(list, pivot + 1, right);
+//	}
+//	
+//}
+//
+//int main()
+//{
+//	StopWatch Watch;
+//	int list1[10] = { 0, };
+//	for (int i = 10; i > 0; i--)
+//	{
+//		list1[10 - i] = i;
+//	}
+//	QuickSort(list1, 0, 9);
+//	for (int k = 0; k < 10; k++)
+//	{
+//		std::cout << " ";
+//		std::cout << list1[k];
+//		std::cout << " ";
+//	}
+//	std::cout << std::endl;
+//	int list2[10] = { 0, };
+//	for (int i = 10; i > 0; i--)
+//	{
+//		list2[10 - i] = i;
+//	}
+//	shellSort(list2,10);
+//	for (int k = 0; k < 10; k++)
+//	{
+//		std::cout << " ";
+//		std::cout << list2[k];
+//		std::cout << " ";
+//	}
+//	std::cout << std::endl;
+//	int listA[1000] = { 0, };
+//	for (int i = 1000; i > 0; i--)
+//	{
+//		listA[1000 - i] = rand();
+//	}
+//	int listB[1000] = { 0, };
+//	for (int i = 1000; i > 0; i--)
+//	{
+//		listB[1000 - i] = listA[1000 - i];
+//	}
+//	Watch.setStartTime();
+//	for (int i = 1000; i > 0; i--)
+//	{
+//		QuickSort(listB, 0, 999);
+//	}
+//	Watch.stop();
+//	std::cout <<"QuickSort" << Watch.getElapsedTime()<<std::endl;
+//	for (int i = 1000; i > 0; i--)
+//	{
+//		listB[1000 - i] = listA[1000 - i];
+//	}
+//	Watch.setStartTime();
+//	for (int i = 1000; i > 0; i--)
+//	{
+//		quick(listB,0, 999);
+//	}
+//	Watch.stop();
+//	std::cout << "quick" << Watch.getElapsedTime() << std::endl;
+//	for (int i = 1000; i > 0; i--)
+//	{
+//		listB[1000 - i] = listA[1000 - i];
+//	}
+//	Watch.setStartTime();
+//	for (int i = 1000; i > 0; i--)
+//	{
+//		shellSort(listB, 1000);
+//	}
+//	Watch.stop();
+//	std::cout <<"ShellSort" << Watch.getElapsedTime() << std::endl;
+//	return 0;
+//}
 #include <iostream>
-#include "StopWatch.h"
-void insertionSort(int v[], int left, int right, int gap)
-{
-	int i, j, temp;
+using namespace std;
 
-	for (i = left + gap; i <= right; i =i + gap)
+static int* buff;
+
+void _mergeSort(int a[], int left,int right)
+{
+	if (left < right)
 	{
-		temp = v[i];
-		for (j = i - gap; j >= left && v[j] > temp; j = j- gap)
-		{
-			v[j + gap] = v[j];
-		}
-		v[j + gap] = temp;
+		int center = (left + right) / 2;
+		int p = 0;
+		int i;
+		int j=0;
+		int k = left;
+		_mergeSort(a, left, center);
+		_mergeSort(a, center + 1, right);
+		for (i = left; i <= center; i++)
+			buff[p++] = a[i];
+		while (i <= right && j < p)
+			a[k++] = (buff[j] <= a[i]) ? buff[j++] : a[i++];
+		while (j < p)
+			a[k++] = buff[j++];
 	}
 }
 
-void shellSort(int v[], int size)
+int mergeSort(int a[], int n)
 {
-	int gap;
-	for (gap = size / 2; gap > 0; gap= gap / 2)
+	if((buff = new int[n]) == NULL)
 	{
-		if ((gap % 2) == 0)
-		{
-			gap++;
-		}
-		for (int i = 0; i < gap; i++)
-		{
-			insertionSort(v, i, size - 1, gap);
-		}
-		
+		return -1;
 	}
-}
-
-void quick(int a[],int left,int right)
-{
-	int pl = left;
-	int pr = right;
-	int x = a[(pl + pr) / 2];
-	do {
-		while (a[pl] < x) pl++;
-		while (a[pr] > x) pr--;
-		if (pl <= pr) {
-			int temp=a[pl];
-			a[pl] = a[pr];
-			a[pr] = temp;
-			pl++;
-			pr--;
-		}
-	} while (pl <= pr);
-	if (left < pr) quick(a,left,pr);
-	if (pl < right) quick(a, pl, right);
-}
-
-void QuickSort(int list[], int left,int right)
-{
-	if (left >= right)
-	{
-		return;
-	}
-	else
-	{
-		int pivotLeft = left+1;
-		int pivot = left;
-		int pivotRight = right;
-		int current = 1;
-		do
-		{
-			if (current)
-			{
-				if (list[pivot] >= list[pivotRight])
-				{
-					int temp = list[pivot];
-					list[pivot] = list[pivotRight];
-					list[pivotRight] = temp;
-					pivot = pivotRight;
-					current = 0;
-				}
-				--pivotRight;
-			}
-			else
-			{
-				if (list[pivot] <= list[pivotLeft])
-				{
-					int temp = list[pivot];
-					list[pivot] = list[pivotLeft];
-					list[pivotLeft] = temp;
-					pivot = pivotLeft;
-					current = 1;
-				}
-				++pivotLeft;
-			}
-		} while (pivotLeft <= pivotRight);
-		QuickSort(list, left, pivot - 1);
-		QuickSort(list, pivot + 1, right);
-	}
-	
+	_mergeSort(a, 0, n - 1);
+	delete[](buff);
+	return 0;
 }
 
 int main()
 {
-	StopWatch Watch;
+	using namespace std;
+
 	int list1[10] = { 0, };
 	for (int i = 10; i > 0; i--)
 	{
 		list1[10 - i] = i;
 	}
-	QuickSort(list1, 0, 9);
+	mergeSort(list1, 10);
 	for (int k = 0; k < 10; k++)
 	{
 		std::cout << " ";
@@ -456,57 +563,5 @@ int main()
 		std::cout << " ";
 	}
 	std::cout << std::endl;
-	int list2[10] = { 0, };
-	for (int i = 10; i > 0; i--)
-	{
-		list2[10 - i] = i;
-	}
-	shellSort(list2,10);
-	for (int k = 0; k < 10; k++)
-	{
-		std::cout << " ";
-		std::cout << list2[k];
-		std::cout << " ";
-	}
-	std::cout << std::endl;
-	int listA[1000] = { 0, };
-	for (int i = 1000; i > 0; i--)
-	{
-		listA[1000 - i] = rand();
-	}
-	int listB[1000] = { 0, };
-	for (int i = 1000; i > 0; i--)
-	{
-		listB[1000 - i] = listA[1000 - i];
-	}
-	Watch.setStartTime();
-	for (int i = 1000; i > 0; i--)
-	{
-		QuickSort(listB, 0, 999);
-	}
-	Watch.stop();
-	std::cout <<"QuickSort" << Watch.getElapsedTime()<<std::endl;
-	for (int i = 1000; i > 0; i--)
-	{
-		listB[1000 - i] = listA[1000 - i];
-	}
-	Watch.setStartTime();
-	for (int i = 1000; i > 0; i--)
-	{
-		quick(listB,0, 999);
-	}
-	Watch.stop();
-	std::cout << "quick" << Watch.getElapsedTime() << std::endl;
-	for (int i = 1000; i > 0; i--)
-	{
-		listB[1000 - i] = listA[1000 - i];
-	}
-	Watch.setStartTime();
-	for (int i = 1000; i > 0; i--)
-	{
-		shellSort(listB, 1000);
-	}
-	Watch.stop();
-	std::cout <<"ShellSort" << Watch.getElapsedTime() << std::endl;
 	return 0;
 }
